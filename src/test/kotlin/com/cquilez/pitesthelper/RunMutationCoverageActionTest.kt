ce3539862b3097b6b84ab97699ab35bf5cd870ba
Carmelo Quílez Gracia <carmelo.quilez@bosonit.com>
2023-09-09 11:59:32 +0200

    Added simple integration tests

--- src/test/kotlin/com/cquilez/pitesthelper/RunMutationCoverageActionTest.kt
+++ src/test/kotlin/com/cquilez/pitesthelper/RunMutationCoverageActionTest.kt
@@ -3,6 +3,7 @@ package com.cquilez.pitesthelper
 import com.cquilez.pitesthelper.actions.RunMutationCoverageAction
 import com.cquilez.pitesthelper.services.ClassService
 import com.intellij.ide.projectView.impl.nodes.ClassTreeNode
+import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
 import com.intellij.openapi.actionSystem.CommonDataKeys.NAVIGATABLE_ARRAY
 import com.intellij.openapi.actionSystem.CommonDataKeys.PROJECT
 import com.intellij.openapi.actionSystem.DataContext
@@ -11,6 +12,9 @@ import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.impl.ModifiableModelCommitter
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.pom.Navigatable
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiManager
 import com.intellij.testFramework.TestActionEvent
 import com.intellij.testFramework.builders.JavaModuleFixtureBuilder
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
@@ -20,6 +24,7 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import java.nio.file.Paths
 
 /**
@@ -45,7 +50,6 @@ class RunMutationCoverageActionTest {
             fixture.setUp()
             fixture.copyDirectoryToProject("sampleProject", "")
 
-            // Add test SourceRoot in src/test/ava
             val module = ModuleManager.getInstance(fixture.project).modules[0]
             val directory = LocalFileSystem.getInstance().findFileByIoFile(Paths.get(fixture.tempDirPath).resolve("src/test/java").toFile())
             val rootModel = ModuleRootManager.getInstance(module).modifiableModel
@@ -87,26 +91,9 @@ class RunMutationCoverageActionTest {
 
     @Test
     fun `Single Main Class selected and Test Class exists, single target class and single test class`() {
-        val psiFile = fixture.configureFromTempProjectFile("src/main/java/com/myproject/ClassA.java")
-
+        val psiFile = fixture.configureFromTempProjectFile("src/main/java/com/myproject/package1/ClassA.java")
         ApplicationManager.getApplication().runReadAction {
-            val dataContext = DataContext {
-                when (it) {
-                    PROJECT.name -> {
-                        return@DataContext fixture.project
-                    }
-
-                    NAVIGATABLE_ARRAY.name -> {
-                        val classTreeNode = ClassTreeNode(fixture.project, ClassService.getPublicClass(psiFile), null)
-                        return@DataContext arrayOf(classTreeNode)
-                    }
-
-                    else -> {
-                        return@DataContext null
-                    }
-                }
-            }
-
+            val dataContext = createDataContext(arrayOf(ClassTreeNode(fixture.project, ClassService.getPublicClass(psiFile), null)))
             val event = TestActionEvent(dataContext)
             val action = RunMutationCoverageAction()
 
@@ -118,33 +105,39 @@ class RunMutationCoverageActionTest {
             val data = RunMutationCoverageAction.mutationCoverageData
             assertTrue(data != null)
             assertTrue(data!!.targetClasses.isNotEmpty())
-            assertEquals("com.myproject.ClassA", data.targetClasses)
+            assertEquals("com.myproject.package1.ClassA", data.targetClasses)
             assertTrue(data.targetTests.isNotEmpty())
-            assertEquals("com.myproject.ClassATest", data.targetTests)
+            assertEquals("com.myproject.package1.ClassATest", data.targetTests)
+        }
+    }
+
+    @Test
+    fun `Single Main Package selected and Test Package exists, single target package and single test package`() {
+        ApplicationManager.getApplication().runReadAction {
+            val directoryTreeNode = findPsiDirectory("src/main/java/com/myproject/package2")
+            val dataContext = createDataContext(arrayOf(directoryTreeNode))
+            val event = TestActionEvent(dataContext)
+            val action = RunMutationCoverageAction()
+
+            action.update(event)
+            assertTrue(event.presentation.isVisible)
+
+            action.actionPerformed(event)
+
+            val data = RunMutationCoverageAction.mutationCoverageData
+            assertTrue(data != null)
+            assertTrue(data!!.targetClasses.isNotEmpty())
+            assertEquals("com.myproject.package2.*", data.targetClasses)
+            assertTrue(data.targetTests.isNotEmpty())
+            assertEquals("com.myproject.package2.*", data.targetTests)
         }
     }
 
     @Test
     fun `Single Test Class selected and Main Class exists, single target class and single test class`() {
-        val psiFile = fixture.configureFromTempProjectFile("src/test/java/com/myproject/ClassATest.java")
-
+        val psiFile = fixture.configureFromTempProjectFile("src/test/java/com/myproject/package2/ClassBTest.java")
         ApplicationManager.getApplication().runReadAction {
-            val dataContext = DataContext {
-                when (it) {
-                    PROJECT.name -> {
-                        return@DataContext fixture.project
-                    }
-
-                    NAVIGATABLE_ARRAY.name -> {
-                        val classTreeNode = ClassTreeNode(fixture.project, ClassService.getPublicClass(psiFile), null)
-                        return@DataContext arrayOf(classTreeNode)
-                    }
-
-                    else -> {
-                        return@DataContext null
-                    }
-                }
-            }
+            val dataContext = createDataContext(arrayOf(ClassTreeNode(fixture.project, ClassService.getPublicClass(psiFile), null)))
 
             val event = TestActionEvent(dataContext)
             val action = RunMutationCoverageAction()
@@ -157,9 +150,66 @@ class RunMutationCoverageActionTest {
             val data = RunMutationCoverageAction.mutationCoverageData
             assertTrue(data != null)
             assertTrue(data!!.targetClasses.isNotEmpty())
-            assertEquals("com.myproject.ClassA", data.targetClasses)
+            assertEquals("com.myproject.package2.ClassB", data.targetClasses)
             assertTrue(data.targetTests.isNotEmpty())
-            assertEquals("com.myproject.ClassATest", data.targetTests)
+            assertEquals("com.myproject.package2.ClassBTest", data.targetTests)
         }
+    }
+
+    @Test
+    fun `Single Test Package selected and Main Package exists, single target package and single test package`() {
+        ApplicationManager.getApplication().runReadAction {
+            val directoryTreeNode = findPsiDirectory("src/test/java/com/myproject/package1")
+            val dataContext = createDataContext(arrayOf(directoryTreeNode))
+            val event = TestActionEvent(dataContext)
+            val action = RunMutationCoverageAction()
+
+            action.update(event)
+            assertTrue(event.presentation.isVisible)
+
+            action.actionPerformed(event)
+
+            val data = RunMutationCoverageAction.mutationCoverageData
+            assertTrue(data != null)
+            assertTrue(data!!.targetClasses.isNotEmpty())
+            assertEquals("com.myproject.package1.*", data.targetClasses)
+            assertTrue(data.targetTests.isNotEmpty())
+            assertEquals("com.myproject.package1.*", data.targetTests)
+        }
+    }
+
+    private fun createDataContext(navigableArray: Array<out Navigatable>): DataContext {
+        return DataContext {
+            when (it) {
+                PROJECT.name -> {
+                    return@DataContext fixture.project
+                }
+
+                NAVIGATABLE_ARRAY.name -> {
+                    return@DataContext navigableArray
+                }
+
+                else -> {
+                    return@DataContext null
+                }
+            }
+        }
+    }
+
+    private fun findPsiDirectory(relativePath: String): PsiDirectoryNode {
+        val module = ModuleManager.getInstance(fixture.project).modules[0]
+        val packageVirtualFile = ModuleRootManager.getInstance(module).contentRoots[0].findFileByRelativePath(relativePath)
+        if (packageVirtualFile == null || !packageVirtualFile.isDirectory) {
+            fail("Fail preparing test: Package com/project was not found")
+        }
+        val psiManager = PsiManager.getInstance(fixture.project)
+        var psiDirectory: PsiDirectory? = null
+        ApplicationManager.getApplication().runReadAction {
+            psiDirectory = psiManager.findDirectory(packageVirtualFile)
+            if (psiDirectory == null || !psiDirectory!!.isDirectory) {
+                fail("Fail preparing test: PsiDirectory com/project was not found")
+            }
+        }
+        return PsiDirectoryNode(fixture.project, psiDirectory!!, null)
     }
 }
